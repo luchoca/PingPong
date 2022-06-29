99aca7126e6a7ec1e2eb3f4509cfdc3bd6945acf
luchoca <luchocastro.3@gmail.com>
2022-06-29 20:35:50 -0300

    Práctica MVC parte 3

--- main.js
+++ main.js
@@ -2,91 +2,108 @@
 //funcion anonima q se ejecuta asi misma para no contaminar el scoope general del proyecto.
 
 (function () {
-  self.Tablero = function (width, height) {
+  self.Board = function (width, height) {
     //self vale algo depende del contexto en el que se encuentre.
     this.width = width;
     this.height = height;
     this.playing = false;
     this.game_over = false;
-    this.paletas = []; // bars
-    this.pelotas = null; // balls
+    this.bars = []; // bars
+    this.balls = null; // balls
   };
-  self.Tablero.prototype = {
+  self.Board.prototype = {
     get elements() {
       //
-      var elements = this.paletas; // retorna las paletas y las pelotas que hay adentro del tablero.
-      elements.push(this.pelota);
+      var elements = this.bars; // retorna las paletas y las pelotas que hay adentro del tablero.
+      elements.push(this.ball);
       return elements;
     },
   };
 })();
 
 (function () {
-  self.Paletas = function (x, y, width, height) {
+  self.Bars = function (x, y, width, height, board) {
     this.x = x; // coordenada
     this.y = y; //coordenada
     this.width = width;
     this.height = height;
-    this.tablero = tablero; //elemento donde se va a dibujar
-    this.tablero.paletas.push(this); //relleno el arreglo , accedo al board accedo al areglo bars y le agrero un nuevo elemepo con push
+    this.board = board; //elemento donde se va a dibujar
+    this.board.bars.push(this);
     this.kind = "rectangle";
+    this.speed = 10; //velocidad que se mueven las paletas
   };
 
-  self.Paletas.prototype = {
+  self.Bars.prototype = {
+    // funciones para mover
     down: function () {
-      // funciones para mover
+      this.y += this.speed;
     },
-    up: function () {},
+    up: function () {
+      this.y += this.speed;
+    },
+    toString: function () {
+      //imprimer en q coordenadas se encuentra, tostring pasa de obejot a string
+      return "x: " + this.x + " y:  " + this.y;
+    },
   };
-})()(
-  //helpper method
-  //clase que va a dibujar el tablero
+})();
+//helpper method , ayuda a los objeto sauqnue no pertenzcan a el
+//clase que va a dibujar el tablero
 
-  function () {
-    self.TableroVista = function (canvas, tablero) {
-      this.canvas = canvas;
-      this.canvas.width = tablero.width;
-      this.canvas.height = tablero.height;
-      this.tablero = tablero;
-      this.contexto = canvas.getContext("2d");
-    };
+(function () {
+  self.BoardView = function (canvas, board) {
+    this.canvas = canvas;
+    this.canvas.width = board.width;
+    this.canvas.height = board.height;
+    this.board = board;
+    this.contexto = canvas.getContext("2d");
+  };
 
-    self.TableroVista.prototype = {
-      //
-      draw: function () {
-        for (var i = this.tablero.element.length - 1; i >= 0; i--) {
-          var el = this.board.element[i]; //elemento a dibujar
+  self.BoardView.prototype = {
+    draw: function () {
+      for (let i = this.board.elements.length - 1; i >= 0; i--) {
+        let el = this.board.elements[i]; //elemento a dibujar
 
-          draw(this.contexto, el);
-        }
-      },
-    };
-
-    function draw(contexto, element) {
-      if (element !== null && element.hasOwnProperty("kind")) {
-        switch (element.kind) {
-          case "rectangule":
-            contexto.fillRect(
-              element.x,
-              element.y,
-              element.width,
-              element.height
-            );
-            break;
-        }
+        draw(this.contexto, el);
+      }
+    },
+  };
+  function draw(contexto, element) {
+    if (element !== null && element.hasOwnProperty("kind")) {
+      switch (element.kind) {
+        case "rectangle":
+          contexto.fillRect(
+            element.x,
+            element.y,
+            element.width,
+            element.height
+          );
+          break;
       }
     }
   }
-)();
+})();
 
-window.addEventListener("load", main);
+//intancias de obejtos
+var board = new Board(800, 400);
+var bars = new Bars(20, 100, 40, 100, board);
+var bars = new Bars(700, 100, 40, 100, board);
+var canvas = document.getElementById("canvas");
+var boardView = new BoardView(canvas, board);
+
+document.addEventListener("keydown", function (ev) {
+  //cada vez que el keydown suceda se va a ejectuar la funcion
+  // console.log(ev.keyCode); //ev --> trae informacion del evento
+  if (ev.keyCode == 38) {
+    bars.up();
+  } else if (ev.keyCode == 40) {
+    bars.down();
+  }
+});
+
+self.addEventListener("load", main);
 
 (function main() {
-  var tablero = new tablero(800, 400);
-  var paletas = new paletas(20, 100, 40, 100, tablero);
-  var paletas = new paletas(700, 100, 40, 100, tablero);
-  var canvas = document.getElementById("canvas");
-  var TableroTamaño = new TableroTamaño(canvas, tablero);
-  TableroTamaño.draw();
+  boardView.draw();
 })();
 function main() {}
